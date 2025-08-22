fc569090a3c0c1c962b8f59bdc9bd61c0bc3ea65
Our Lady of Fatima Church, Sewri <ladyoffatimachurch@gmail.com>
2025-08-22 23:37:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -50,7 +50,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const payload = { name, email, date, time };
 
     try {
-      await fetch('https://script.google.com/macros/s/AKfycbzvTTPmesEdeU7hi5dv4aeOxCoqBj_2jQ2ankTvg6pQrQYAdEb1klsmdjzHfW0l628/exec', {
+      await fetch('https://script.google.com/macros/s/AKfycbwcQqLO-iweY70EC2rxKUkiJ6hLVgJ0g2KanZ8XOvaWE9VFL32VJo6tIyt6_eukvqU8DA/exec', {
         method: 'POST',
         mode: 'no-cors',
         headers: {
